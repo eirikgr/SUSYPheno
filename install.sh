36ae0e0c439afbcd08c80065c4d68cfd4c80f6b5
eirikgr <eirikgr@fys.uio.no>
2019-03-18 11:21:28 +0100

    Updated installation script

--- install.sh
+++ install.sh
@@ -1,1 +1,15 @@
-cd ./HDECAY
+# This script compiles all the needed packages
+export SUSYPHENO_PATH=$PWD
+echo $SUSYPHENO_PATH
+cd $SUSYPHENO_PATH/HDECAY
+make
+cd $SUSYPHENO_PATH/MICROMEGA/micromegas_5.0.8/
+make
+cd $SUSYPHENO_PATH/SUSYHIT/
+make
+cd $SUSYPHENO_PATH/DARKSUSY/darksusy-5.1.1/
+./configure
+make
+cd $SUSYPHENO_PATH
+
+
