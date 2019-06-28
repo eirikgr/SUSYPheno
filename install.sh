4f1388e873a2211a1fddaff8a7e48d38c1da772d
eirikgr <eirikgr@fys.uio.no>
2019-06-28 11:15:33 +0200

    Updated install script to clean directories before compiling

--- install.sh
+++ install.sh
@@ -2,14 +2,20 @@
 export SUSYPHENO_PATH=$PWD
 echo $SUSYPHENO_PATH
 cd $SUSYPHENO_PATH/HDECAY
+make clean
 make
 cd $SUSYPHENO_PATH/MICROMEGA/micromegas_3.5.5/
+make clean
+mv CalcHEP_src/FlagsForSh CalcHEP_src/FlagsForSh_obso
 gmake
 cd $SUSYPHENO_PATH/MICROMEGA/micromegas_3.5.5/MSSM/
 gmake main=micromegas_mini_slha.c
 cd $SUSYPHENO_PATH/SUSYHIT/
+make clean
 make
 cd $SUSYPHENO_PATH/DARKSUSY/darksusy-5.1.1/
+make uninstall
+make distclean
 ./configure
 make
 make install
